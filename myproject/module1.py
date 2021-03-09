0ce47d3de669630db8c31dffcd874cafcb260944
fedem-p <federico.puppo@tum.de>
2021-03-09 11:04:36 +0000

    test better doc strings

--- myproject/module1.py
+++ myproject/module1.py
@@ -2,7 +2,41 @@ import numpy as np
 
 def summation(a: int, b: int):
     """
-    Sums two integers
+    Compute the arithmetic sum of two `int`.
+
+    Parameters
+    ----------
+    a : `int`
+        integer number, if `a` is not `int` a conversion is attempted
+    b : `int`
+        integer number, if `a` is not `int` a conversion is attempted
+
+    Returns
+    -------
+    m : `int`
+
+    See Also
+    --------
+    average : Weighted average
+    std, var, nanmean, nanstd, nanvar
+
+    Notes
+    -----
+    The arithmetic mean is the sum of the elements along the axis divided
+    by the number of elements.
+    Note that for floating-point input, the mean is computed using the
+    same precision the input has.  Depending on the input data, this can
+    cause the results to be inaccurate, especially for `float32` (see
+    example below).  Specifying a higher-precision accumulator using the
+    `dtype` keyword can alleviate this issue.
+    By default, `float16` results are computed using `float32` intermediates
+    for extra precision.
+
+    Examples
+    --------
+    >>> a = summation(3,5)
+    >>> a
+    8
     """
     return a+b 
 
